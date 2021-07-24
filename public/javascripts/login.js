7413354d001b2d3b7f460930339ed550c21791c5
Yuki <yuki_matsubara@outlook.com>
2021-07-24 12:59:45 -0700

    webpage and style done

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -3,7 +3,7 @@ const emailInput = document.querySelector(".emailInput");
 const passwordInput = document.querySelector(".passwordInput");
 
 const login = () => {
-  alert("login successfull :)");
+  alert("Login successfull :)");
   //go back to the home page
   location.pathname = "/";
 };
@@ -12,5 +12,5 @@ loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
   //validation check
   (emailInput.value === "" || passwordInput.value === "") ?
-    alert("please input your credential") : login();
+    alert("Please input your credential") : login();
 });
